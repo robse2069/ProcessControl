b5dc496f88768e4c75f3373ba571f5915fc62ee7
robert.schedlik <robert.schedlik@smartmechatronics.de>
2026-09-02 15:27:38 +0200

    corrects testcase

--- test/SmokeTestLibrary.py
+++ test/SmokeTestLibrary.py
@@ -5,7 +5,7 @@ class SmokeTestLibrary:
     """Keywords used by the cross-machine Robot Framework smoke test."""
 
     def framework_is_available(self):
-        return 
+        return True
 
     def pcberry_is_available(self):
         try:
--- tools/run_smoketest.sh
+++ tools/run_smoketest.sh
@@ -3,7 +3,7 @@ git pull
 
 cd /home/pi/docker/processcontrol
 docker compose up -d
-docker compose exec robot robot test/smoke_test.robot
+docker compose run --rm --network host robot robot test/smoke_test.robot
 
 cd /home/pi/docker/processcontrol
 docker compose down
